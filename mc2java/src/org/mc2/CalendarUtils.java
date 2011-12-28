1b2abe59066bf6a9fcfcc09c7ddb1c88128edf8b
marcoc1712 <marcoc1712@gmail.com>
2011-12-28 17:49:50 +0000

    Utilities

--- mc2java/src/org/mc2/CalendarUtils.java
+++ mc2java/src/org/mc2/CalendarUtils.java
@@ -1,11 +1,5 @@
 package org.mc2;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import java.util.Calendar;
 import java.util.TimeZone;
 
