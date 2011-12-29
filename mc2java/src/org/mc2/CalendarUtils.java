c486fd990d5c6ac0a70d7d3805cfb6074128a95b
marcoc1712@gmail.com <marcoc1712@gmail.com@fa57e5e7-4cd0-bbd0-907d-46747ff077c5>
2011-12-29 01:24:39 +0000

    Utilities

--- mc2java/src/org/mc2/CalendarUtils.java
+++ mc2java/src/org/mc2/CalendarUtils.java
@@ -10,7 +10,7 @@ import java.util.TimeZone;
 public class CalendarUtils {
 
 
-    public static String calcDuration(Long durms){
+    public static String calcDurationString(Long durms){
         
         if (durms ==null) return "";
 
@@ -31,5 +31,71 @@ public class CalendarUtils {
         }
 
         return dur;
+    }
+    public static String calcDurationString(int sectors){
+        
+        return calcDurationString(calcDurationInMillis(sectors));
+    }
+    
+    public static long calcDurationInMillis(int sectors) {
+        return new Long(sectors)*1000/75;
+    }
+    public static long calcDurationInMillis(String durStr) throws MC2Exception {
+        
+        if (durStr == null) return 0;
+        try {
+                Calendar cal = Calendar.getInstance();
+                TimeZone tx = TimeZone.getDefault();
+                int offset = tx.getOffset(cal.getTimeInMillis());
+                cal.setTimeInMillis(0);
+                
+                int sec=0;
+                int min=0;
+                int hour=0;
+                
+                long time;
+                
+ 
+                String[] split = durStr.split(":");
+                if (split.length == 1){
+                      sec = Integer.parseInt(split[0]);
+                      cal.set(Calendar.SECOND,sec );
+                }
+                else if (split.length == 2) {
+                      min = Integer.parseInt(split[0]);
+                      sec = Integer.parseInt(split[1]);
+                      cal.set(Calendar.MINUTE,min);
+                      cal.set(Calendar.SECOND, sec);
+                } else if (split.length == 3) {
+                      hour = Integer.parseInt(split[0]);
+                      min = Integer.parseInt(split[1]);
+                      sec = Integer.parseInt(split[2]);
+                      cal.set(Calendar.HOUR, hour);
+                      cal.set(Calendar.MINUTE, min);
+                      cal.set(Calendar.SECOND, sec);
+                }
+                else{
+                    throw new MC2Exception("invalid time format, should be HH:MM:SS");
+                }
+                /*
+             * Used calendar just as a entry validation. Strange stuffs with 
+             * dayligth saving offset...
+             */
+                long timecal = cal.getTimeInMillis();
+                long timeOffset = timecal+offset;
+                
+                time = (sec +min*60+hour*60*60)*1000;
+                
+                return time;
+                
+        } catch (NumberFormatException ex){
+            throw new MC2Exception(ex);
+        }
+    }
+    public static int calcDurationInSector(long durms){
+         return ((int)durms*75/1000);
+    }
+    public static int calcDurationInSector(String durSt) throws MC2Exception{
+         return calcDurationInSector(calcDurationInMillis(durSt));
     }
 }
